f4d7231d369182c9357704acb857e2108e10774a
Arushi <as72604@uga.edu>
2026-07-24 16:58:28 -0400

    Initial colors template

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Colors.blue;
+  static const secondary = Colors.orange;
+  static const background = Color(0xFFF5F5F5);
+  static const text = Colors.black87;
+}
+
+//(note to self: this is just an example of how to use the colors defined in AppColors class)
+//Anywhere in the app:
+//import '../utils/colors.dart';
+//backgroundColor: AppColors.primary
