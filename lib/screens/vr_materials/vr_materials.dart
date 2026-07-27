3ea26617fe98aac8fe2dcbb88298183871001f7e
Arushi <as72604@uga.edu>
2026-07-27 19:05:50 -0400

    Updated initial VR materials page with a couple images and a short blurb

--- lib/screens/vr_materials/vr_materials.dart
+++ lib/screens/vr_materials/vr_materials.dart
@@ -1,4 +1,6 @@
+//import statements
 import 'package:flutter/material.dart';
+import '../../widgets/lazy_image.dart';
 
 class VRMaterials extends StatelessWidget {
   const VRMaterials({super.key});
@@ -7,7 +9,7 @@ class VRMaterials extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("VR Materials"),
+        title: const Text("Warnell VR Lab"),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
@@ -27,34 +29,37 @@ class VRMaterials extends StatelessWidget {
               ),
 
               const SizedBox(height: 25),
-
-              // Image
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-               // child: Image.asset(
-               //   'assets/images/vr_lab_demo.jpg',
-               //   width: double.infinity,
-               //   height: 250,
-               //   fit: BoxFit.cover,
-              //  ),
-              ),
-
-              const SizedBox(height: 25),
-
-              // Description
+              // Description of overall content on this page
               const Text(
                 "Thank you for visiting the VR Materials page!"
-                " Here, you'll find a collection of resources and materials related to the Warnell VR Lab. "
-                "You can use this area to describe the content of the page, "
-                "provide instructions, or display information about the image "
-                "shown above. The text automatically wraps to multiple lines "
-                "and remains readable on different screen sizes.",
+                " Here, you'll find a collection of resources and materials related to the Warnell VR Lab. ",
                 textAlign: TextAlign.justify,
                 style: TextStyle(
                   fontSize: 18,
                   height: 1.6,
                 ),
               ),
+
+              const SizedBox(height: 25),
+               //Image of students wearing VR headsets in the classroom
+              const LazyImage(assetPath: 'lib/assets/images/perseus_vr_headsets_classroom.jpg'),
+
+              const SizedBox(height: 25),
+              // Description of actions for the user to take on this page regarding VR headsets
+              const Text(
+                "The Warnell VR Lab interns have created a YouTube page for 360-degree forestry videos."
+                " You can check out VR headsets for yourself or a group of people using the checkout form and supplemental documents below.",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 18,
+                  height: 1.6,
+                ),
+              ),
+
+              const SizedBox(height: 25),
+              //Image of VR evolution graphic from UGA PMRC
+              const LazyImage(assetPath: 'lib/assets/images/perseus_vr_evolution_graphic.jpeg'),
+
             ],
           ),
         ),
