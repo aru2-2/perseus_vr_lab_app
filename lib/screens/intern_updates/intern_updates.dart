8f4f9dd81804a2f9c05d7b38fbd227d2f021b309
Arushi <as72604@uga.edu>
2026-07-28 13:29:06 -0400

    Added Whitehall field trip image and code comments

--- lib/screens/intern_updates/intern_updates.dart
+++ lib/screens/intern_updates/intern_updates.dart
@@ -1,16 +1,34 @@
 //import statements
 import 'package:flutter/material.dart';
 import '../../widgets/lazy_image.dart';
+import '../../widgets/theme_toggle_button.dart';
 
+//creates the Intern Updates page of the app
 class InternUpdates extends StatelessWidget {
-  const InternUpdates({super.key});
+  
+  //toggles between light and dark mode
+  final bool isDarkMode;
+  final VoidCallback onToggleTheme;
 
+  const InternUpdates({
+    super.key,
+    required this.isDarkMode,
+    required this.onToggleTheme,
+  });
+
+//builds the Intern Updates page widget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Warnell VR Lab"),
         centerTitle: true,
+        actions: [
+          ThemeToggleButton(
+            isDarkMode: isDarkMode,
+            onToggleTheme: onToggleTheme,
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -40,6 +58,23 @@ class InternUpdates extends StatelessWidget {
                 ),
               ),
 
+
+              const SizedBox(height: 25),
+              // Image of Whitehall field day DBH activity for the interns
+              const LazyImage(assetPath: 'lib/assets/images/perseus_whitehall_dbh_activity.JPG'),
+
+              const SizedBox(height: 25),
+              // Description of the forest VR equipment image
+              const Text(
+                "In this image, some of the PERSEUS Project interns are competing to identify the correct tree diameter using DBH measurements.",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 18,
+                  height: 1.6,
+                ),
+              ),
+
+
               const SizedBox(height: 25),
               // Image of forest VR equipment
               const LazyImage(assetPath: 'lib/assets/images/perseus_vr_camera_woods.jpeg'),
@@ -47,8 +82,7 @@ class InternUpdates extends StatelessWidget {
               const SizedBox(height: 25),
               // Description of the forest VR equipment image
               const Text(
-                "This image showcases the VR equipment used by the Warnell VR Lab interns to capture tree imagery at Whitehall Forest.(fix description)",
-                textAlign: TextAlign.justify,
+                "This image showcases the equipment used by the Warnell VR Lab interns to capture tree imagery at Whitehall Forest.",             textAlign: TextAlign.justify,
                 style: TextStyle(
                   fontSize: 18,
                   height: 1.6,
