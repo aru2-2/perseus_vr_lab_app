1ffa26f85880cad33b757ec17885713b3116f9a3
Arushi <as72604@uga.edu>
2026-07-28 13:15:56 -0400

    Added test for toggle button behavior

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,4 +15,25 @@ void main() {
 
     expect(tester.takeException(), isNull);
   });
+
+  testWidgets('theme toggle is available on each screen', (tester) async {
+    await tester.pumpWidget(const WarnellVRLab());
+
+    final routes = [
+      '/',
+      '/vr_materials',
+      '/posters_and_publications',
+      '/intern_updates',
+      '/robot',
+    ];
+
+    for (final route in routes) {
+      await tester.pumpWidget(const WarnellVRLab());
+      final context = tester.element(find.text('Warnell VR Lab'));
+      Navigator.of(context).pushNamed(route);
+      await tester.pumpAndSettle();
+
+      expect(find.byTooltip('Switch to dark mode'), findsOneWidget);
+    }
+  });
 }
