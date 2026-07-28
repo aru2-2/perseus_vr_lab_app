207931f67225fc429d6dfae72196ad22f64e02ba
Arushi <as72604@uga.edu>
2026-07-28 13:52:20 -0400

    Increased color scheme throughout the app

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-
-  static const primary = Color(0xFF2E7D32);
-  static const secondary = Color(0xFFFF9800);
-  static const backgroundLight = Color(0xFFF5F5F5);
-  static const backgroundDark = Color(0xFF121212);
-  static const textLight = Color(0xFF1F1F1F);
-  static const textDark = Color(0xFFF5F5F5);
+  static const primary = Color(0xFF2E6F40);
+  static const secondary = Color(0xFF4F8A5B);
+  static const backgroundLight = Color(0xFFF6F8F4);
+  static const backgroundDark = Color(0xFF121A15);
+  static const textLight = Color(0xFF173124);
+  static const textDark = Color(0xFFF3F7F2);
   static const cardLight = Color(0xFFFFFFFF);
-  static const cardDark = Color(0xFF1E1E1E);
-  static const mutedText = Color(0xFF757575);
+  static const cardDark = Color(0xFF1D2A23);
+  static const mutedText = Color(0xFF6E7A72);
   static const buttonText = Color(0xFFFFFFFF);
+  static const borderLight = Color(0xFFDDE7E0);
+  static const borderDark = Color(0xFF31453A);
 }
