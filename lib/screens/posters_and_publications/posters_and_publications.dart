492806bc86279bd6080b7a8debac0df735e301bd
Arushi <as72604@uga.edu>
2026-08-03 12:53:48 -0400

    Edited photos and captions to make screen cleaner

--- lib/screens/posters_and_publications/posters_and_publications.dart
+++ lib/screens/posters_and_publications/posters_and_publications.dart
@@ -67,8 +67,7 @@ class PostersAndPublications extends StatelessWidget {
               // Description of Purdue poster presentation in 2026
               const Text(
                 "Joey, Addy, and Maadhavan presented about the Warnell VR Lab at the Application and Solutions in Digital Forestry International Conference at Purdue University."
-                " They showcased the capabilities of our VR Lab as experiential learning in immersive solutions."
-                " (fix and add more details)",
+                " They showcased the capabilities of our VR Lab as experiential learning in immersive solutions.",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 18,
@@ -85,8 +84,7 @@ class PostersAndPublications extends StatelessWidget {
               // Description of CURO 2026 poster presentation
               const Text(
                 "Joey, Addy, and Maadhavan presented about forestry education in a virtual world at CURO 2026, showcasing the capabilities of our VR Lab and the potential applications of our research." 
-                " The poster highlighted their work in 360-degree virtual reality and LiDAR simulations in the classroom."
-                " (fix and add more details)",
+                " The poster highlighted their work in 360-degree virtual reality and LiDAR simulations in the classroom.",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 18,
@@ -96,15 +94,14 @@ class PostersAndPublications extends StatelessWidget {
 
               const SizedBox(height: 25),
               // Image of UGA presentation in 2025
-              const LazyImage(assetPath: 'lib/assets/images/perseus_uga_presentation_2025.jpeg'),
+              const LazyImage(assetPath: 'lib/assets/images/perseus_uga_presentation_2025.JPG'),
 
 
               const SizedBox(height: 25),
               // Description of UGA presentation in 2025
               const Text(
-                "Maadhavan presented about forests in a virtual world at the 2025 PERSEUS Annual Meeting at the University of Georgia." 
-                " His presentation demonstrated initial research into using virtual reality headsets to advance the PERSEUS Project objectives."
-                " (fix and add more details)",
+                "Some of the Warnell VR Lab interns presented about forests in a virtual world at the 2025 PERSEUS Annual Meeting at the University of Georgia." 
+                " Their presentation demonstrated initial research into using virtual reality headsets to advance the PERSEUS Project objectives.",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 18,
