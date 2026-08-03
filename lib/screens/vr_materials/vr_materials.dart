4d7daddd67a088067a527abf9a314ec04b042302
Arushi <as72604@uga.edu>
2026-08-03 16:20:03 -0400

    Added more images and supplemental document links

--- lib/screens/vr_materials/vr_materials.dart
+++ lib/screens/vr_materials/vr_materials.dart
@@ -4,10 +4,13 @@ import 'package:url_launcher/url_launcher.dart';
 import '../../widgets/lazy_image.dart';
 import '../../widgets/theme_toggle_button.dart';
 
-//hyperlinks
+//hyperlinks for external resources
 const String youtubeChannelUrl = 'https://www.youtube.com/@warnellvrlab';
 const String vrCheckoutFormUrl = 'https://docs.google.com/forms/d/e/1FAIpQLScW3RJlXfUuNiWHifQgQZdld7Q4p0t0hceFo1n5-zgMY8RBkg/viewform?usp=header';
 const String vrCheckinFormUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSdFWFjoDly9Bu8waKSgZOEy41RW_f5XkFOXEXo04Qdkmxm1iA/viewform?pli=1';
+const String vrHeadsetInstructions = 'https://drive.google.com/file/d/19xtkUkBc7KOkP2EGIz9adFN497YsOfJa/view';
+const String unityDevelopmentGuide = 'https://docs.google.com/document/d/17YXO3ov6evBGmPRMIyoop3esnh1T9jPAtCr4ibtvloI/edit?tab=t.0#heading=h.wbd25knfx8ig';
+const String mlcTechLending = 'https://www.libs.uga.edu/mlc/techlending';
 
 //creates the VR Materials page of the app
 class VRMaterials extends StatelessWidget {
@@ -70,7 +73,7 @@ class VRMaterials extends StatelessWidget {
               const SizedBox(height: 25),
               // Description of actions for the user to take on this page regarding VR headsets
               const Text(
-                "The Warnell VR Lab interns have created a YouTube page for 360-degree forestry videos.",
+                "The Warnell VR Lab interns have created a YouTube page for 360-degree forestry videos. Check it out below!",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 18,
@@ -91,8 +94,12 @@ class VRMaterials extends StatelessWidget {
                 },
                 //icon and label to display on the button
                 icon: const Icon(Icons.play_circle_fill_rounded),
-                label: const Text('Visit the YouTube channel!'),
+                label: const Text('Visit the YouTube channel'),
               ),
+
+              const SizedBox(height: 25),
+               //Image of teachers and faculty wearing VR headsets in the classroom
+              const LazyImage(assetPath: 'lib/assets/images/perseus_faculty_vr_headsets.JPG'),
 
               //description of VR headset checkout form
               const SizedBox(height: 25),
@@ -118,7 +125,7 @@ class VRMaterials extends StatelessWidget {
                 },
                 //icon and label to display on the button
                 icon: const Icon(Icons.headset_rounded),
-                label: const Text('Visit the VR Check-out Form'),
+                label: const Text('VR Check-out Form'),
               ),
 
               //description of VR headset checkin form
@@ -145,13 +152,19 @@ class VRMaterials extends StatelessWidget {
                 },
                 //icon and label to display on the button
                 icon: const Icon(Icons.headset_off_rounded),
-                label: const Text('Visit the VR Check-in Form'),
+                label: const Text('VR Check-in Form'),
               ),
 
-              //description of VR history infographic from PMRC
+              const SizedBox(height: 25),
+               //Image of Dr. Lowe experimenting with a VR headset
+              const LazyImage(assetPath: 'lib/assets/images/perseus_drlowe_vr_headset.JPG'),
+
+              
+              //description of VR headset supplemental materials
               const SizedBox(height: 25),
               const Text(
-                "This infographic illustrates the history of virtual reality, from its first prototype to a tool for immersive forestry education at Warnell.",
+                "Below are some supplemental documents containing information about the VR headsets."
+                " If you have any questions or feedback, please fill out the contact form located on the home page and we will be in touch!",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 18,
@@ -159,9 +172,69 @@ class VRMaterials extends StatelessWidget {
                 ),
               ),
 
-              const SizedBox(height: 50),
+              //links to supplemental materials will open in an external browser when clicked
+              //General VR Headset Instructions
+              const SizedBox(height: 18),
+              TextButton.icon(
+                //button opens VR headset instructions form in external browser
+                onPressed: () async {
+                  final uri = Uri.parse(vrHeadsetInstructions);
+                  //throws exception if URL cannot be launched
+                  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+                    throw Exception('Could not launch $uri');
+                    }
+                },
+                //icon and label to display on the button
+                icon: const Icon(Icons.info_rounded),
+                label: const Text('VR Headset Instructions'),
+              ),
+
+              //Unity Development Guide
+              const SizedBox(height: 18),
+              TextButton.icon(
+                //button opens Unity development guide in external browser
+                onPressed: () async {
+                  final uri = Uri.parse(unityDevelopmentGuide);
+                  //throws exception if URL cannot be launched
+                  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+                    throw Exception('Could not launch $uri');
+                    }
+                },
+                //icon and label to display on the button
+                icon: const Icon(Icons.book_rounded),
+                label: const Text('Unity Development Guide'),
+              ),
+
+              //MLC Tech Lending Policies for UGA
+              const SizedBox(height: 18),
+              TextButton.icon(
+                //button opens MLC tech lending policies in external browser
+                onPressed: () async {
+                  final uri = Uri.parse(mlcTechLending);
+                  //throws exception if URL cannot be launched
+                  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+                    throw Exception('Could not launch $uri');
+                    }
+                },
+                //icon and label to display on the button
+                icon: const Icon(Icons.policy_rounded),
+                label: const Text('MLC Tech Lending Policy Page'),
+              ),
+              
+              //description of VR history infographic from PMRC
+              //const SizedBox(height: 25),
+              //const Text(
+              //  "This infographic illustrates the history of virtual reality, from its first prototype to a tool for immersive forestry education at Warnell.",
+              //  textAlign: TextAlign.center,
+              //  style: TextStyle(
+              //    fontSize: 18,
+              //    height: 1.6,
+              //  ),
+              //),
+
+              //const SizedBox(height: 50),
               //Image of VR evolution graphic from UGA PMRC
-              const LazyImage(assetPath: 'lib/assets/images/perseus_vr_evolution_graphic.jpeg'),
+              //const LazyImage(assetPath: 'lib/assets/images/perseus_vr_evolution_graphic.jpeg'),
 
             ]
           ),
