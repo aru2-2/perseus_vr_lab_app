579ab3f7ad76f70f96f0b37d1e2efd241389343a
Arushi <as72604@uga.edu>
2026-08-05 13:34:51 -0400

    Added card style design to the VR materials page

--- lib/screens/vr_materials/vr_materials.dart
+++ lib/screens/vr_materials/vr_materials.dart
@@ -1,6 +1,7 @@
 //import statements
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import '../../utils/colors.dart';
 import '../../widgets/lazy_image.dart';
 import '../../widgets/theme_toggle_button.dart';
 
@@ -14,7 +15,64 @@ const String mlcTechLending = 'https://www.libs.uga.edu/mlc/techlending';
 
 //creates the VR Materials page of the app
 class VRMaterials extends StatelessWidget {
-  
+  //contains each card and the contents within them
+  static const List<_VRMaterialsEntry> _entries = [
+    //youtube channel
+    _VRMaterialsEntry(
+      imagePath: 'lib/assets/images/perseus_vr_headsets_classroom.jpg',
+      description:
+          'The Warnell VR Lab interns have created a YouTube page for 360-degree forestry videos. Check it out by clicking on this card!',
+      actionLabel: 'Visit the YouTube channel',
+      actionUrl: youtubeChannelUrl,
+      actionIcon: Icons.play_circle_fill_rounded,
+    ),
+    //VR headset checkout form
+    _VRMaterialsEntry(
+      imagePath: 'lib/assets/images/perseus_faculty_vr_headsets.JPG',
+      description:
+          'You can also check out VR headsets for yourself or a group of people using the check-out form below.',
+      actionLabel: 'VR Check-out Form',
+      actionUrl: vrCheckoutFormUrl,
+      actionIcon: Icons.headset_rounded,
+    ),
+    //VR headset checkin form
+    _VRMaterialsEntry(
+      imagePath: 'lib/assets/images/perseus_drlowe_vr_headset.JPG',
+      description:
+          'Remember to fill out the check-in form when you return the headsets!',
+      actionLabel: 'VR Check-in Form',
+      actionUrl: vrCheckinFormUrl,
+      actionIcon: Icons.headset_off_rounded,
+    ),
+    //VR headset instructions
+    _VRMaterialsEntry(
+      imagePath: 'lib/assets/images/perseus_drone_in_forest.JPG',
+      description:
+          'This is a quick and easy instruction page on how to use the VR headsets.',
+      actionLabel: 'VR Headset Instructions',
+      actionUrl: vrHeadsetInstructions,
+      actionIcon: Icons.info_rounded,
+    ),
+    //Unity development guide
+    _VRMaterialsEntry(
+      imagePath: 'lib/assets/images/perseus_drone_in_forest.JPG',
+      description:
+          'If you want to create your own VR videos, read this guide to get started!',
+      actionLabel: 'Unity Development Guide',
+      actionUrl: unityDevelopmentGuide,
+      actionIcon: Icons.book_rounded,
+    ),
+    //MLC tech lending policy
+    _VRMaterialsEntry(  
+      imagePath: 'lib/assets/images/perseus_drone_in_forest.JPG',
+      description:
+          'At UGA, our VR headsets are subject to the technology lending policy set by the Miller Learning Center (MLC). Check it out here.',
+      actionLabel: 'MLC Tech Lending Policy',
+      actionUrl: mlcTechLending,
+      actionIcon: Icons.policy_rounded,
+    ),
+  ];
+
   //toggles between light and dark mode
   final bool isDarkMode;
   final VoidCallback onToggleTheme;
@@ -28,7 +86,10 @@ class VRMaterials extends StatelessWidget {
   //builds the VR Materials page widget
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
     return Scaffold(
+      //top bar of the app
       appBar: AppBar(
         title: const Text("Warnell VR Lab"),
         centerTitle: true,
@@ -67,176 +128,129 @@ class VRMaterials extends StatelessWidget {
               ),
 
               const SizedBox(height: 25),
-               //Image of students wearing VR headsets in the classroom
-              const LazyImage(assetPath: 'lib/assets/images/perseus_vr_headsets_classroom.jpg'),
 
-              const SizedBox(height: 25),
-              // Description of actions for the user to take on this page regarding VR headsets
-              const Text(
-                "The Warnell VR Lab interns have created a YouTube page for 360-degree forestry videos. Check it out below!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  height: 1.6,
+              //Creates a list of cards using the defined card parameters above
+              ...List.generate(
+                _entries.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: _VRMaterialsResourceCard(
+                    entry: _entries[index],
+                    isDark: isDark,
+                  ),
                 ),
               ),
-
-              //YouTube channel button will open the hyperlink in an external browser when pressed
-              const SizedBox(height: 18),
-              TextButton.icon(
-                //button opens YouTube channel in external browser
-                onPressed: () async {
-                  final uri = Uri.parse(youtubeChannelUrl);
-                  //throws exception if URL cannot be launched
-                  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
-                    throw Exception('Could not launch $uri');
-                    }
-                },
-                //icon and label to display on the button
-                icon: const Icon(Icons.play_circle_fill_rounded),
-                label: const Text('Visit the YouTube channel'),
-              ),
-
-              const SizedBox(height: 25),
-               //Image of teachers and faculty wearing VR headsets in the classroom
-              const LazyImage(assetPath: 'lib/assets/images/perseus_faculty_vr_headsets.JPG'),
-
-              //description of VR headset checkout form
-              const SizedBox(height: 25),
-              const Text(
-                "You can also check out VR headsets for yourself or a group of people using the check-out form below.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  height: 1.6,
-                ),
-              ),
-
-              //VR headset checkout form hyperlink will open in an external browser when pressed
-              const SizedBox(height: 18),
-              TextButton.icon(
-                //button opens VR headset checkout form in external browser
-                onPressed: () async {
-                  final uri = Uri.parse(vrCheckoutFormUrl);
-                  //throws exception if URL cannot be launched
-                  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
-                    throw Exception('Could not launch $uri');
-                    }
-                },
-                //icon and label to display on the button
-                icon: const Icon(Icons.headset_rounded),
-                label: const Text('VR Check-out Form'),
-              ),
-
-              //description of VR headset checkin form
-              const SizedBox(height: 25),
-              const Text(
-                "Remember to fill out the check-in form when you return the headsets!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  height: 1.6,
-                ),
-              ),
-
-              //VR headset checkin form hyperlink will open in an external browser when pressed
-              const SizedBox(height: 18),
-              TextButton.icon(
-                //button opens VR headset checkin form in external browser
-                onPressed: () async {
-                  final uri = Uri.parse(vrCheckinFormUrl);
-                  //throws exception if URL cannot be launched
-                  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
-                    throw Exception('Could not launch $uri');
-                    }
-                },
-                //icon and label to display on the button
-                icon: const Icon(Icons.headset_off_rounded),
-                label: const Text('VR Check-in Form'),
-              ),
-
-              const SizedBox(height: 25),
-               //Image of Dr. Lowe experimenting with a VR headset
-              const LazyImage(assetPath: 'lib/assets/images/perseus_drlowe_vr_headset.JPG'),
-
-              
-              //description of VR headset supplemental materials
-              const SizedBox(height: 25),
-              const Text(
-                "Below are some supplemental documents containing information about the VR headsets."
-                " If you have any questions or feedback, please fill out the contact form located on the home page and we will be in touch!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  height: 1.6,
-                ),
-              ),
-
-              //links to supplemental materials will open in an external browser when clicked
-              //General VR Headset Instructions
-              const SizedBox(height: 18),
-              TextButton.icon(
-                //button opens VR headset instructions form in external browser
-                onPressed: () async {
-                  final uri = Uri.parse(vrHeadsetInstructions);
-                  //throws exception if URL cannot be launched
-                  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
-                    throw Exception('Could not launch $uri');
-                    }
-                },
-                //icon and label to display on the button
-                icon: const Icon(Icons.info_rounded),
-                label: const Text('VR Headset Instructions'),
-              ),
-
-              //Unity Development Guide
-              const SizedBox(height: 18),
-              TextButton.icon(
-                //button opens Unity development guide in external browser
-                onPressed: () async {
-                  final uri = Uri.parse(unityDevelopmentGuide);
-                  //throws exception if URL cannot be launched
-                  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
-                    throw Exception('Could not launch $uri');
-                    }
-                },
-                //icon and label to display on the button
-                icon: const Icon(Icons.book_rounded),
-                label: const Text('Unity Development Guide'),
-              ),
-
-              //MLC Tech Lending Policies for UGA
-              const SizedBox(height: 18),
-              TextButton.icon(
-                //button opens MLC tech lending policies in external browser
-                onPressed: () async {
-                  final uri = Uri.parse(mlcTechLending);
-                  //throws exception if URL cannot be launched
-                  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
-                    throw Exception('Could not launch $uri');
-                    }
-                },
-                //icon and label to display on the button
-                icon: const Icon(Icons.policy_rounded),
-                label: const Text('MLC Tech Lending Policy Page'),
-              ),
-              
-              //description of VR history infographic from PMRC
-              //const SizedBox(height: 25),
-              //const Text(
-              //  "This infographic illustrates the history of virtual reality, from its first prototype to a tool for immersive forestry education at Warnell.",
-              //  textAlign: TextAlign.center,
-              //  style: TextStyle(
-              //    fontSize: 18,
-              //    height: 1.6,
-              //  ),
-              //),
-
-              //const SizedBox(height: 50),
-              //Image of VR evolution graphic from UGA PMRC
-              //const LazyImage(assetPath: 'lib/assets/images/perseus_vr_evolution_graphic.jpeg'),
-
             ]
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//variables for each card entry
+class _VRMaterialsEntry {
+  final String imagePath;
+  final String description;
+  final String actionLabel;
+  final String actionUrl;
+  final IconData actionIcon;
+
+  const _VRMaterialsEntry({
+    required this.imagePath,
+    required this.description,
+    required this.actionLabel,
+    required this.actionUrl,
+    required this.actionIcon,
+  });
+}
+
+//creates the VR Materials page of the app
+class _VRMaterialsResourceCard extends StatelessWidget {
+  final _VRMaterialsEntry entry;
+  final bool isDark;
+
+  const _VRMaterialsResourceCard({
+    required this.entry,
+    required this.isDark,
+  });
+
+  //builds the card widget that is used for each entry
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+        side: BorderSide(
+          color: isDark ? AppColors.borderDark : AppColors.borderLight,
+          width: 1.2,
+        ),
+      ),
+      color: isDark ? AppColors.cardDark : AppColors.cardLight,
+      child: InkWell(
+        onTap: () async {
+          final uri = Uri.parse(entry.actionUrl);
+          if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+            throw Exception('Could not launch $uri');
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            //order of elements in each card from top to bottom: hyperlink, image, text description
+            children: [
+              //hyperlink at the top of each card
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: isDark
+                        ? const Color(0xFF24312A)
+                        : const Color(0xFFF2F7F0),
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  child: TextButton.icon(
+                    onPressed: () async {
+                      final uri = Uri.parse(entry.actionUrl);
+                      if (!await launchUrl(
+                          uri,
+                          mode: LaunchMode.externalApplication)) {
+                        throw Exception('Could not launch $uri');
+                      }
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor:
+                          isDark ? AppColors.textDark : AppColors.textLight,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 14,
+                        vertical: 10,
+                      ),
+                    ),
+                    icon: Icon(entry.actionIcon),
+                    label: Text(entry.actionLabel),
+                  ),
+                ),
+              ),
+              //spacing between the hyperlink and image
+              const SizedBox(height: 18),
+              //image in the center of each card
+              LazyImage(assetPath: entry.imagePath),
+              //spacing between the image and text description
+              const SizedBox(height: 16),
+              //text description of resource in the card
+              Text(
+                entry.description,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  height: 1.6,
+                  color: isDark ? AppColors.textDark : AppColors.textLight,
+                ),
+              ),
+            ],
           ),
         ),
       ),
